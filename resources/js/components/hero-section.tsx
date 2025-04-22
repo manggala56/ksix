b9d18b2b0e6c9e5bb6d85149e9c344601bad969e
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2025-04-22 17:22:57 +0700

    UI: Landing Status

--- resources/js/components/hero-section.tsx
+++ resources/js/components/hero-section.tsx
@@ -5,34 +5,36 @@ import { Button } from './ui/button';
 export default function HeroSection() {
     return (
         <>
-            <div className="">
-                <div className="flex items-center justify-center">
-                    <img
-                        src="/images/logo-k16-no-bg.png"
-                        className="w-80 opacity-100 duration-1000 starting:-translate-y-20 starting:opacity-0"
-                        alt=""
-                    />
+            <div className="overflow-hidden">
+                <div className="">
+                    <div className="flex items-center justify-center">
+                        <img
+                            src="/images/logo-k16-no-bg.png"
+                            className="w-80 opacity-100 duration-1000 starting:-translate-y-20 starting:opacity-0"
+                            alt=""
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="mb-5">
-                <div className="font-tarrget-italic text-center text-3xl opacity-100 duration-1000 starting:translate-x-20 starting:opacity-0">
-                    rental PS K16 🎮
+                <div className="mb-5">
+                    <div className="font-tarrget-italic text-center text-3xl opacity-100 duration-1000 starting:translate-x-20 starting:opacity-0">
+                        rental PS K16 🎮
+                    </div>
+                    <div className="font-montserrat-regular mb-1 text-center text-sm text-neutral-300 duration-1000 starting:-translate-x-20 starting:opacity-0">
+                        Kami memiliki banyak pilihan game yang bisa kalian pilih, Kami siap melayani 24 JAM Penuh!, segera kunjungi rental kami untuk
+                        merasakan main PS yang seru, nyaman dan Murah!.
+                    </div>
                 </div>
-                <div className="font-montserrat-regular mb-1 text-center text-sm text-neutral-300 duration-1000 starting:-translate-x-20 starting:opacity-0">
-                    Kami memiliki banyak pilihan game yang bisa kalian pilih, Kami siap melayani 24 JAM Penuh!, segera kunjungi rental kami untuk
-                    merasakan main PS yang seru, nyaman dan Murah!.
-                </div>
-            </div>
-            <div className="mb-5 flex justify-center gap-3 duration-1000 starting:translate-y-20 starting:opacity-0">
-                {/* <Button variant="secondary">
+                <div className="mb-5 flex justify-center gap-3 duration-1000 starting:translate-y-20 starting:opacity-0">
+                    {/* <Button variant="secondary">
                     <Clock /> <div>Cek Billing</div>
                 </Button> */}
-                <Link href={route('booking')}>
-                    <Button>
-                        <Book />
-                        Booking Sekarang
-                    </Button>
-                </Link>
+                    <Link href={route('booking')}>
+                        <Button>
+                            <Book />
+                            Booking Sekarang
+                        </Button>
+                    </Link>
+                </div>
             </div>
         </>
     );
